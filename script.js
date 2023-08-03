f6182b68871110e0240965808122e92576b1d347
Vitor <logonrmlocal@fiap.com.br>
2023-08-03 20:37:22 -0300

    Criando a classificação

---
+++ script.js
@@ -0,0 +1,8 @@
+//Definindo a função com arrow functions
+const mudar=()=>{
+    let novo = document.getElementById('idNome');
+
+//Chamando pelo ID
+document.getElementById('titulo').required=true;
+document.getElementById('titulo').innerHTML= novo.value;
+}
